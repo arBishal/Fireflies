af63fa2f5da23431abe0777ce499ecae5ddfbd63
arBishal <arbishal.319@gmail.com>
2025-06-28 23:30:46 +0600

    setup tailwind and cleanup css

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <script setup></script>
 
 <template>
-  <header></header>
-
-  <main>Fireflies</main>
+  <main className="flex items-center justify-center bg-black text-white h-dvh">Fireflies</main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-}
-</style>
+<style scoped></style>
